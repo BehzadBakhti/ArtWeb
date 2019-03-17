94fee99472156eb06a8cc5194460a736dc9785f2
Behzad Bakhti <behzad.bakhti@gmail.com>
2019-03-17 23:17:20 +0330

    factory setup and data seeder

--- database/factories/ProductFactory.php
+++ database/factories/ProductFactory.php
@@ -2,8 +2,14 @@
 
 use Faker\Generator as Faker;
 
+
 $factory->define(App\Model\Product::class, function (Faker $faker) {
     return [
+        'name'=>$faker-> word,
+        'detail'=>$faker-> paragraph,
+        'stock'=>$faker-> numberBetween(0,100),
+        'price'=>$faker-> numberBetween(100,1000),
+        'discount'=>$faker-> numberBetween(0,30),
         //
     ];
 });
